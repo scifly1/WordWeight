a8ae164df1ac52f21b4e07f8cbef7da7add5087d
Paul Elms <scifly@uprocera.co.uk>
2011-01-17 11:17:35 +0000

    Refactored word_weight.py.
    Moved all handling of the new odt file into a class.
    Moved a lot of code into discrete functions to aid readability.

---
+++ ODT.py
@@ -0,0 +1,148 @@
+
+# Copyright 2010 - 2011 Paul Elms
+# 
+# This file is part of word_weight.
+# 
+# It is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# word_weight is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with word_weight.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from xml.dom import minidom
+import shutil
+import tempfile
+import zipfile
+
+
+class ODT(object):
+    '''
+    ODT is a class representing an odt file.  It has methods to create a
+    new odt file, to add text to and set the font sizes of that text in the 
+    odt file.
+    '''
+    
+    odt_template_filename = 'template-blank.odt'    
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        odt = zipfile.ZipFile(self.odt_template_filename, 'r')
+        self.odt_ls = odt.namelist()
+        #Create temp dir for constuction of a new odt file.
+        self.tmp_odt_dir = tempfile.mkdtemp()
+        odt.extractall(self.tmp_odt_dir)
+        odt.close()
+        
+        self.content_path = self.tmp_odt_dir + '/content.xml'
+        
+        self.__get_content_xml()
+        
+    def __get_content_xml(self):
+        """
+        Returns the DOM of the content.xml file in the odt archive.
+        """
+        self.content_xml = minidom.parse(self.content_path)
+        
+    def parse_word_list(self, word_list):
+        """
+        Takes a list of words and the weighting to be applied to each one
+        as a list of tuples.
+        Parses the each word and weighting and then adds each them to the
+        content.xml file of the odt archive.  It creates the correct style 
+        elements for each weighting/font size.
+        """
+        self.__setup_styles_and_para_elements()
+        self.size_list = []
+        
+        for word in word_list:
+            size = word[1]
+            if size not in self.size_list:
+                self.size_list.append(size)
+                self.__create_new_style_node(size)
+            #Check each word has a trailing space
+            text = self.__check_spaces(word[0])
+            #Create the text:span node from each piece of text
+            #and its associated style
+            self.__create_text_nodes(text, size)
+            
+        
+        
+    def __setup_styles_and_para_elements(self):
+        """
+        Capture the automatic-styles and paragraph elements that hold the font size
+        data and the text respectively.
+        """
+        self.styles = self.content_xml.getElementsByTagName('office:automatic-styles')
+        self.paragraph = self.content_xml.getElementsByTagName('text:p')
+        
+    def __create_new_style_node(self,size):
+        """
+        Creates a new style node and text-properties node and appends 
+        them to the style element
+        """
+        elem = self.content_xml.createElement('style:style')
+        elem.setAttribute('style:name', str.format('T{0}', size))
+        elem.setAttribute('style:family','text')
+        self.styles[0].appendChild(elem)
+
+        elem = self.content_xml.createElement('style:text-properties')
+        pt_size = str.format('{0}pt', size)
+        elem.setAttribute('fo:font-size', pt_size)
+        elem.setAttribute('style:font-size-asian', pt_size)
+        elem.setAttribute('style:font-size-complex', pt_size)
+        self.styles[0].lastChild.appendChild(elem) 
+        
+    def __check_spaces(self, word):
+        """
+        Each word needs a space after it to be correctly formatted 
+        in the re-constructed sentance.
+        """
+        text = word[0]
+        if text[-1:] != ' ':
+            text = word + " " 
+        return text   
+        
+    def __create_text_nodes(self, text, size):
+        """
+        Each text span node contains the text in that span and its 
+        associated style
+        """ 
+        elem = self.content_xml.createElement('text:span')
+        elem.setAttribute('text:style-name',str.format('T{0}', size))
+        text_elem = self.content_xml.createTextNode(text)
+        elem.appendChild(text_elem)
+        self.paragraph[0].appendChild(elem)
+        
+    def save(self,filename):
+        """
+        Saves the new content.xml to a file and then recreates 
+        the odt archive
+        """
+        content = open(self.content_path,'w')
+        self.content_xml.writexml(content, encoding='utf-8')
+        content.close()
+        
+        new_odt = zipfile.ZipFile(filename, 'w')
+        for file in self.odt_ls:
+            new_odt.write(self.tmp_odt_dir + '/' + file, file)
+            
+        new_odt.close()
+        
+    def finish(self):
+        """
+        Clean up.
+        Temp odt directory is deleted here.
+        """
+        shutil.rmtree(self.tmp_odt_dir)
+        
+        
--- word_weight.py
+++ word_weight.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-# Copyright 2010 Paul Elms
+# Copyright 2010 - 2011 Paul Elms
 # 
 # word_weight is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,59 +15,71 @@
 # You should have received a copy of the GNU General Public License
 # along with word_weight.  If not, see <http://www.gnu.org/licenses/>.
 
-import zipfile
-from xml.dom import minidom
 from optparse import OptionParser
+from xml.dom import minidom
+import ODT
 import tempfile
-import shutil
+import zipfile
 
-#TODO Break up into functions to improve readability
 
-# Set up optparse to get ods filename and to supply an output filename.
 def __read_cmd_args():
+    """
+    Set up the Option Parser to get the ods filename and output
+    filename from the command line parameters.
+    """
     usage = "usage: ./word_weight.py -f ODS_FILE [-o OUTPUT_FILE]\n\nTo run supply the ods file with the data and an optional output filename."
     parser = OptionParser(usage=usage)
     parser.add_option("-f", "--filename", metavar="ODS_FILE", action="store", type="string", dest="ods_filename", help="Specify an .ODS_FILE to get data from.")
     parser.add_option("-o", "--output-file", metavar="OUTPUT_FILE", action="store", type="string", dest="output_filename", help="Specify an OUTPUT_FILE.")
     return parser.parse_args()
 
-options, args = __read_cmd_args()
 
-#open ods spreadsheet and get each word and its size in a list of tuples.
-ods_file = options.ods_filename
-ods = zipfile.ZipFile(ods_file,'r')
-tmp_ods_dir = tempfile.mkdtemp()
-ods.extract('content.xml',tmp_ods_dir)
-ods.close()
+def __get_rows(filename):
+    """
+    Open the ods spreadsheet and get each row in to a list to return.
+    """
+    ods_file = filename
+    ods = zipfile.ZipFile(ods_file,'r')
+    tmp_ods_dir = tempfile.mkdtemp()
+    ods.extract('content.xml',tmp_ods_dir)
+    ods.close()
 
-ods_xml = minidom.parse(tmp_ods_dir + '/content.xml')
-rows = ods_xml.getElementsByTagName('table:table-row')
+    ods_xml = minidom.parse(tmp_ods_dir + '/content.xml')
+    rows = ods_xml.getElementsByTagName('table:table-row')
 
-word_list = []
-#Create word and associated size list from ods file.
-for row in rows:
-    if row.childNodes.length > 1:
-        if row.childNodes[1].getAttribute('office:value-type') == 'float':
-            text = row.childNodes[0].childNodes[0].childNodes[0].nodeValue
-            size = row.childNodes[1].childNodes[0].childNodes[0].nodeValue
+    return rows
+
+def __parse_rows(rows):
+    """
+    Parse rows from the ods file and return a list of tuples of each word and
+    its associated weighting.
+    """
+    word_list = []
+    for row in rows:
+        if row.childNodes.length > 1:
+            if row.childNodes[1].getAttribute('office:value-type') == 'float':
+                text = row.childNodes[0].childNodes[0].childNodes[0].nodeValue
+                size = row.childNodes[1].childNodes[0].childNodes[0].nodeValue
     
-            word_list.append((text, size))
+                word_list.append((text, size))
+    return word_list
+                
+    
+#Program starts here..
+options, args = __read_cmd_args()
+rows = __get_rows(options.ods_filename)
+word_list = __parse_rows(rows)
 
-#Extract the template odt file and parse content.xml
-odt_template = 'template-blank.odt'
-odt = zipfile.ZipFile(odt_template, 'r')
-odt_ls = odt.namelist()
-tmp_odt_dir = tempfile.mkdtemp()
-odt.extractall(tmp_odt_dir)
-odt.close()
-odt_path = tmp_odt_dir + '/content.xml'
-odt_xml = minidom.parse(odt_path)
+new_odt = ODT.ODT()
+new_odt.parse_word_list(word_list)
+new_odt.save(options.output_filename)
+new_odt.finish()
 
-styles = odt_xml.getElementsByTagName('office:automatic-styles')
-paragraph = odt_xml.getElementsByTagName('text:p')
+
+"""
 
 #Run through list of font sizes to find all unique sizes and add them to the odt template
-size_list = []
+
 for word in word_list:
     size = word[1]
     if size not in size_list:
@@ -88,6 +100,8 @@ for word in word_list:
     text = word[0]
     if text[-1:] != ' ':
         text = word[0] + " "
+        
+        
     #Create the text:span node from each piece of text and its associated style
     elem = odt_xml.createElement('text:span')
     elem.setAttribute('text:style-name',str.format('T{0}', size))
@@ -104,6 +118,8 @@ for name in odt_ls:
     new_odt.write(tmp_odt_dir + '/' + name, name)
 
 new_odt.close()
-shutil.rmtree(tmp_ods_dir)
+
 shutil.rmtree(tmp_odt_dir)
+"""
+
 
